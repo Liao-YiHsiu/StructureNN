2dad5d7e2490c0b67853f3d7ed31d7d17f304a2a
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2015-02-02 12:42:17 +0800

    slice test data into pieces and train

--- src/svm_hmm.sh
+++ src/svm_hmm.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 . ../timit/path.sh
 
-FEAT=../timit/data-fmllr-tri3/train/feats.scp
+FEAT=../timit/data-fmllr-tri3/test/feats.scp
+DIR=data
 CORES=7
 
-rm -rf data
-mkdir data
-split -l $(( `wc -l < $FEAT` / CORES )) $FEAT data/split
+rm -rf $DIR 
+mkdir $DIR 
+split -l $(( `wc -l < $FEAT` / CORES + 1)) $FEAT $DIR/split
 
-for line in data/split*
+for line in $DIR/split*
 do
    mv $line ${line}.scp
 done
 
 
-for line in data/split*.scp
+for line in $DIR/split*.scp
 do
    copy-feats scp:$line ark:- | \
    nnet-forward --no-softmax=true --prior-scale=1.0 --feature-transform=../timit/exp/dnn4_pretrain-dbn_dnn_smbr/final.feature_transform --class-frame-counts=../timit/exp/dnn4_pretrain-dbn_dnn_smbr/ali_train_pdf.counts --use-gpu=no ../timit/exp/dnn4_pretrain-dbn_dnn_smbr/final.nnet ark,s,cs:- ark:- | \
-   ./svm_hmm /corpus/timit/ ../timit/conf/phones.60-48-39.map ark,s,cs:- ${line}.out 
+   ./svm_hmm /corpus/timit/ ../timit/conf/phones.60-48-39.map ark,s,cs:- ${line}.out  
    ../svm_hmm/svm_hmm_learn -c 5 -e 0.5 ${line}.out ${line}.model &>${line}.log &
    echo "SVM training start..................................."
 done
