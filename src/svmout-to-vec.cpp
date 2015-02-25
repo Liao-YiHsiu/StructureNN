aa40727f816878fb3696c82a3a69c7eb3d2264ad
loach <nee0224256667@yahoo.com.tw>
2015-02-25 16:33:39 +0800

    add some toolkits

---
+++ src/svmout-to-vec.cpp
@@ -0,0 +1,65 @@
+#include "nnet/nnet-trnopts.h"
+#include "nnet/nnet-nnet.h"
+#include "nnet/nnet-loss.h"
+#include "nnet/nnet-randomizer.h"
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "base/timer.h"
+#include "cudamatrix/cu-device.h"
+#include "svm.h"
+#include <sstream>
+
+using namespace std;
+using namespace kaldi;
+using namespace kaldi::nnet1;
+
+int main(int argc, char *argv[]) {
+  
+  try {
+    string usage;
+    usage.append("Convert SVM out to int32 vector using label to re-align\n")
+       .append("Usage: ").append(argv[0]).append(" [options] <svm-tags> <label-rspecifier> <path-wspecifier>\n")
+       .append("e.g.: \n")
+       .append(" ").append(argv[0]).append(" test.tags ark:test.lab ark:path.ark \n");
+
+    ParseOptions po(usage.c_str());
+
+    po.Read(argc, argv);
+
+    if (po.NumArgs() != 3) {
+      po.PrintUsage();
+      exit(1);
+    }
+
+    string svm_tags_file = po.GetArg(1),
+      label_rspecifier   = po.GetArg(2),
+      path_wspecifier    = po.GetArg(3);
+
+
+    ifstream  fin(svm_tags_file.c_str());
+    SequentialInt32VectorReader  label_reader(label_rspecifier);
+    Int32VectorWriter            path_writer(path_wspecifier);
+
+    int N = 0;
+    for ( ; !label_reader.Done(); label_reader.Next(), N++) {
+
+       const vector<int32> &label = label_reader.Value(); 
+
+       vector<int32> tmp(label.size());
+       for(int i = 0; i < label.size(); ++i)
+          assert(fin >> tmp[i]);
+
+       path_writer.Write(label_reader.Key(), tmp);
+    }
+
+    // final message
+    KALDI_LOG << "Finish " << N << " utterances.";
+
+    return 0;
+  } catch(const std::exception &e) {
+    std::cerr << e.what();
+    return -1;
+  }
+}
+
+
---
+++ src/trim-path.cpp
@@ -0,0 +1,65 @@
+#include "nnet/nnet-trnopts.h"
+#include "nnet/nnet-nnet.h"
+#include "nnet/nnet-loss.h"
+#include "nnet/nnet-randomizer.h"
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "base/timer.h"
+#include "cudamatrix/cu-device.h"
+#include "svm.h"
+#include <sstream>
+
+using namespace std;
+using namespace kaldi;
+using namespace kaldi::nnet1;
+
+int main(int argc, char *argv[]) {
+  
+  try {
+    string usage;
+    usage.append("Trim Path(Remove duplicate part of a vector)\n")
+       .append("Usage: ").append(argv[0]).append(" [options] <path-rspecifier> <path-wspecifier>\n")
+       .append("e.g.: \n")
+       .append(" ").append(argv[0]).append(" ark:path1.ark ark:path2.ark \n");
+
+    ParseOptions po(usage.c_str());
+
+    po.Read(argc, argv);
+
+    if (po.NumArgs() != 2) {
+      po.PrintUsage();
+      exit(1);
+    }
+
+    string path_rspecifier  = po.GetArg(1),
+      path_wspecifier       = po.GetArg(2);
+
+
+    SequentialInt32VectorReader    path_reader(path_rspecifier);
+    Int32VectorWriter              path_writer(path_wspecifier);
+
+    int N = 0;
+    for ( ; !path_reader.Done(); path_reader.Next(), N++) {
+       const vector<int32>& arr = path_reader.Value();
+       vector<int32> tmp;
+       int32 prev = arr[0];
+       tmp.push_back(arr[0]);
+
+       for(int i = 1; i < arr.size(); ++i){
+          if(prev != arr[i]){
+             prev = arr[i];
+             tmp.push_back(arr[i]);
+          }
+       }
+       path_writer.Write(path_reader.Key(), tmp);
+    }
+    KALDI_LOG << "Finish " << N << " utterance.";
+
+    return 0;
+  } catch(const std::exception &e) {
+    std::cerr << e.what();
+    return -1;
+  }
+}
+
+
