4c6b17b368be0a7e15b753b4b9d597723fc35c69
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2015-07-11 13:04:04 +0800

    draw python bug fixed

--- draw.py
+++ draw.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 import sys
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 def main(argv):
-   if len(argv) != 2 :
+   if len(argv) != 3 :
       sys.exit("Usage: draw.py <file> <dir>")
 
    filename = argv[1]
@@ -14,6 +16,7 @@ def main(argv):
       ax  = fig.add_subplot(111)
       ax.plot(points[utt][0], points[utt][1], 'ro')
       fig.savefig('{}/{}.png'.format(dirname, utt))
+      plt.close(fig)
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+   main(sys.argv)
